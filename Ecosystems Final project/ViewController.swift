c6fc6dfd5a9cb3cb255d700c45bf5d4785b9c128
Belal E <balolyman1234@gmail.com>
2017-05-17 11:33:56 +0300

    start

--- Ecosystems Final project/ViewController.swift
+++ Ecosystems Final project/ViewController.swift
@@ -8,13 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var TableView1: UITableView!
+    
+    var question1 = "Which is the order of the flow of energy in an energy pyramid from bottom to top?"
+    var answers1 = ["Primary Consumer,Tertiary Consumer,Secondary Consumer,Producer,Decomposer","Producer,Primary Consumer,Secondary Consumer,Tertiary Consumer,Decomposer.", "Producer,Secondary Consumer,Tertiary Consumer, Decomposer"]
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        TableView1.dataSource = self
+        TableView1.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = answers1[indexPath.row]
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
